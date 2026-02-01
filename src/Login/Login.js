e441e8ddd7aebfc52075009abc929abd9bcfd468
JaySzn992000 <121809806+JaySzn992000@users.noreply.github.com>
2026-02-01 10:31:42 +0530

    Update Login.js

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -53,7 +53,7 @@ return;
 
 try {
 
-const response = await fetch("http://localhost:3001/fetchlogin", {
+const response = await fetch("https://namasya.onrender.com/fetchlogin", {
 method: "POST",
 headers: {
 "Content-type": "application/json",
